dd76669fbecb0aae535e5c1af8ace32b15316356
romsik <romsi@ac.sce.ac.il>
2016-11-22 19:45:28 +0200

    Update add-note.ts
    asd

--- add-note.ts
+++ add-note.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera } from 'ionic-native';
-
+ //sdasd
 
 import {NotesData} from "../../providers/notes-data";
 
